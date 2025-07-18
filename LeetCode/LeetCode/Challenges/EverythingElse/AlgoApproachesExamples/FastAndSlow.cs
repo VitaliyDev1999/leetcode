858b7d2283aa29681ab8932dca2300bc4a26d3cc
vdeineha <deinega.vit1@gmail.com>
2025-07-18 09:41:27 +0200

    Added examples of basic algo

---
+++ LeetCode/LeetCode/Challenges/EverythingElse/AlgoApproachesExamples/FastAndSlow.cs
@@ -0,0 +1,28 @@
+﻿using LeetCode.Challenges.LeetCode;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.Challenges.EverythingElse.AlgoApproachesExamples
+{
+    /* Example of problems
+     * Remove Duplicates from Sorted Array
+     * Move Zeroes
+     * Partition Array by Condition
+     */
+    public class FastAndSlow
+    {
+        //int slow = 0;
+
+        //for (int fast = 0; fast<nums.Length; fast++)
+        //{
+        //    if (nums[fast] != valueToRemove)
+        //    {
+        //        nums[slow] = nums[fast];
+        //        slow++;
+        //    }
+        //}
+    }
+}
---
+++ LeetCode/LeetCode/Challenges/EverythingElse/AlgoApproachesExamples/TwoPointers.cs
@@ -0,0 +1,30 @@
+﻿namespace LeetCode.Challenges.EverythingElse.AlgoApproachesExamples
+{
+    /* Example of problems
+     * Two Sum II
+     * Valid Palindrome
+     * Container With Most Water
+     */
+    public class TwoPointers
+    {
+        public int[] TwoPointersExample(int[] nums, int target)
+        {
+            int left = 0;
+            int right = nums.Length - 1;
+
+            while (left < right)
+            {
+                int sum = nums[left] + nums[right];
+
+                if (sum == target)
+                    return new int[] { left, right };
+                else if (sum < target)
+                    left++;
+                else
+                    right--;
+            }
+
+            return Array.Empty<int>();
+        }
+    }
+}
